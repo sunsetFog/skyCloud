eda1051f2d011239d2ed8a62c68ddb10fc2812ed
ivory <jsivory@itcom666.com>
2023-11-02 15:33:36 +0800

    2023-11-2 15:34

--- means/modular/domUnit/themeSkin.js
+++ means/modular/domUnit/themeSkin.js
@@ -7,26 +7,10 @@ export default {
         const type = sessionStorage.getItem('theme_skin') || '';
         this.changeTheme(type);
     },
-    // 修改html标签的类名，从而改变:root根样式
     changeTheme(type) {
         sessionStorage.setItem('theme_skin', type);
-        document.documentElement.className = '';
-        switch (type) {
-            case 'white':
-                document.documentElement.classList.add('white');
-                break;
-            case 'black':
-                document.documentElement.classList.add('black');
-                break;
-            case 'blue':
-                document.documentElement.classList.add('blue');
-                break;
-            default:
-                document.documentElement.classList.remove('white');
-                break;
-        }
-    },
-    changeTheme2(type) {
+        // 修改html标签的类名
+        document.documentElement.className = type;
         // html设置属性
         document.documentElement.setAttribute('data-theme', type);
     }
