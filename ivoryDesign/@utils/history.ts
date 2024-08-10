078e2183373e58061ecc5e3209e0e30d910c560f
ivory <jsivory@itcom666.com>
2024-08-10 21:40:29 +0800

    2024-8-10 21:41

--- ivoryDesign/@utils/history.ts
+++ ivoryDesign/@utils/history.ts
@@ -1,13 +1,26 @@
 /*
     用于跳转
-    这里方式1，不是全局的
-    方式2: react-router-dom版本6 全局注入history
 
-    使用：
+    使用不了：
     import history from '@/@energy/ivoryDesign/@utils/history';
     history.push(value.key);
-    问题：
-    react-router-dom版本6中，跳转不重新渲染，没用这个跳
+
+
+    react-router-dom版本6
+    已报废
+    import { withRouter } from 'react-router-dom';
+
+    想注入history，找不到参数
+    import { Router } from 'react-router-dom';
+    <Router history={history}></Router>
+
+    import { useNavigate } from 'react-router-dom';
+    useNavigate是函数组件用的
+
+    BrowserRouter自动创建并管理 history 实例，不能使用createBrowserHistory
+    上面三个都用不了，类组件怎么跳转啊？？
+
+
 */
 import {createBrowserHistory} from 'history';
 const history = createBrowserHistory();
