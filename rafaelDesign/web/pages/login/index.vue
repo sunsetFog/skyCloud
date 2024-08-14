282f576d490030086553dabeee57abed00e39f56
ivory <jsivory@itcom666.com>
2024-08-14 21:47:38 +0800

    2024-8-14 21:48

--- rafaelDesign/web/pages/login/index.vue
+++ rafaelDesign/web/pages/login/index.vue
@@ -12,26 +12,41 @@
                         <form>
                             <div class="account">
                                 <span>账号:</span>
-                                <input style="background: none;backgroundColor: none !important;"
-                                    v-model.trim="account_number" maxlength="20" placeholder="请输入账号" name="username"
-                                    autocomplete="new-password" />
+                                <input v-model.trim="account_number" maxlength="20" type="text" placeholder="请输入账号"
+                                    name="username" autocomplete="username" />
                             </div>
                             <div class="password-enter">
                                 <span>密码:</span>
                                 <!--
                                 浏览器自动填充？
+                                    type="text"和type="password"
                                     name="username"和name="password"
-                                    autocomplete="new-password"禁止浏览器自动填充到表单
-                                登录后，input弹出浏览器记住密码弹框？
-                                    readonly使获取光标
+                                    对比
+                                        旧密码
+                                        autocomplete="new-password"用于新密码输入场景，防止浏览器自动填充旧密码
+                                        验证码
+                                        autocomplete="off"禁用浏览器的自动填充功能
+                                    对比
+                                        autocomplete="current-password"推荐
+                                        autocomplete="password"导致username没自动填充提示窗口
+                                    确保没有干扰自动填充的属性，readonly或disabled
+
+
+
+                                使浏览器在用户登录后弹出“记住密码”或“更新密码”的提示框?
+                                    不要使用阻止默认提交行为，别用@submit.prevent
+                                    浏览器自动填充标准ok
+                                    post请求
+
+
+
                                 按钮可以加loading加载效果
                                 enter键盘触发事件
                                     方式1: 密码input输入框加@keyup.enter.native="signIn"，但是要获取焦点
                                     方式2: 监听键盘事件
                             -->
-                                <input :type="eyeType" maxlength="12" v-model.trim="password_number" placeholder="请输入登陆密码"
-                                    name="password" autocomplete="new-password" :readonly="readonly"
-                                    @focus="readonly = false" @blur="readonly = true" />
+                                <input :type="eyeType" maxlength="12" v-model.trim="password_number"
+                                    placeholder="请输入登陆密码" name="password" autocomplete="current-password" />
                                 <div @click="eyeMeans">
                                     <img v-if="eyeType == 'text'" class="zhengyan" src="./img/zhengyan.png" />
                                     <img v-else class="biyan" src="./img/biyan.png" />
@@ -42,11 +57,11 @@
                             <div class="rectangle" @click="remember_checked = !remember_checked"><img
                                     v-show="remember_checked" src="./img/jizhu.png" /></div>
                             <span>记住密码</span>
-                            <!-- <span @click="forgetPassword">忘记密码</span> -->
+                            <span @click="forgetPassword">忘记密码</span>
                         </div>
                         <div class="login-register">
-                            <span @click="signIn"></span>
-                            <span @click="rapidRegistration('register')"></span>
+                            <button @click="signIn"></button>
+                            <button @click="rapidRegistration('register')"></button>
                         </div>
                         <div class="recommend"><span></span><span>推荐最优路线</span><span></span></div>
                         <div class="optimal" @click="rapidRegistration('line')"></div>
@@ -69,8 +84,7 @@ export default defineComponent({
             account_number: '',// 登陆账号
             password_number: '',// 登陆密码
             remember_checked: false,
-            eyeType: 'password',
-            readonly: true
+            eyeType: 'password'
         }
     },
     created() {
@@ -79,11 +93,11 @@ export default defineComponent({
 
         // this.catchMice();
         // 记住密码
-        // if (this.$means.getCookie("remember_account")) {
-        //     this.account_number = this.$means.getCookie("remember_account");
-        //     this.password_number = this.$means.getCookie("remember_password", true);
-        //     this.remember_checked = true;
-        // }
+        if (this.$means.getCookie("remember_account")) {
+            this.account_number = this.$means.getCookie("remember_account");
+            this.password_number = this.$means.getCookie("remember_password", true);
+            this.remember_checked = true;
+        }
 
 
         this.keyEnter();
@@ -425,11 +439,12 @@ export default defineComponent({
                     width: 100%;
                     height: 50px;
 
-                    span {
+                    button {
                         cursor: pointer;
+                        background: none;
                     }
 
-                    span:nth-of-type(1) {
+                    button:nth-of-type(1) {
                         width: 48%;
                         height: 50px;
                         display: inline-block;
@@ -443,11 +458,11 @@ export default defineComponent({
                         .mixin_image(url('./img/denglu.jpg'));
                     }
 
-                    span:nth-of-type(1):hover {
+                    button:nth-of-type(1):hover {
                         .mixin_image(url('./img/denglu_hover.jpg'));
                     }
 
-                    span:nth-of-type(2) {
+                    button:nth-of-type(2) {
                         width: 48%;
                         height: 50px;
                         display: inline-block;
@@ -461,7 +476,7 @@ export default defineComponent({
                         .mixin_image(url('./img/kuaisuzhuce.jpg'));
                     }
 
-                    span:nth-of-type(2):hover {
+                    button:nth-of-type(2):hover {
                         .mixin_image(url('./img/kuaisuzhuce_hover.jpg'));
                     }
                 }
