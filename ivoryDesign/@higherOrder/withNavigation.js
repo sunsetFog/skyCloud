c001108ae06ba84740f3c276169362f182f898db
ivory <1456300078@qq.com>
2026-03-30 04:14:18 +0800

    2026-3-30 4:14

--- ivoryDesign/@higherOrder/withNavigation.js
+++ ivoryDesign/@higherOrder/withNavigation.js
@@ -4,7 +4,11 @@ import {useNavigate} from 'react-router-dom';
     高阶组件
     反向继承
 
-    使类组件能用hooks，设置跳转的props值
+    react类组件如何跳转？？
+    "react": "^18.2.0",
+    "react-router-dom": "^6.21.2",这个6版本已删withRouter
+
+    使类组件能用hooks，设置跳转的props值，函数组件里的类子组件的传参数
     this.props.navigate('/home/reactClass/exJump?id=6');
 
     函数组件的跳转
